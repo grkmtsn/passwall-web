807c1e073b68cbd17f4c7c2e8317de65404c0e60
Kaan <kaangokdemir01@gmail.com>
2020-04-19 10:56:39 +0300

    Base path added to signin and some code refactoring

--- pages/login.js
+++ pages/login.js
@@ -1,50 +1,79 @@
-import * as React from "react"
-import { Form, FormItem, Input, SubmitButton } from "formik-antd"
-import { Formik } from "formik"
-import { UserOutlined, LockOutlined } from "@ant-design/icons"
-import * as Yup from "yup"
-import Router from "next/router"
+import * as React from 'react'
+import { Form, FormItem, Input, SubmitButton } from 'formik-antd'
+import { Formik } from 'formik'
+import { UserOutlined, LockOutlined, GlobalOutlined } from '@ant-design/icons'
+import * as Yup from 'yup'
+import Router from 'next/router'
 
-import fetch from "../libs/fetch"
+import fetch from '../libs/fetch'
 
 const LoginSchema = Yup.object().shape({
-  Username: Yup.string().required("Required"),
-  Password: Yup.string().required("Required")
+  Username: Yup.string().required('Required'),
+  Password: Yup.string().required('Required'),
 })
 
 function LoginPage() {
   const onSubmit = async (values, actions) => {
     try {
-      const { token } = await fetch("/auth/signin", {
-        method: "POST",
-        body: JSON.stringify(values)
+      const { token } = await fetch('/auth/signin', {
+        method: 'POST',
+        body: JSON.stringify(values),
       })
-      localStorage.setItem("TOKEN", token)
-      Router.push("/")
+      localStorage.setItem('TOKEN', token)
+      Router.push('/')
     } catch (e) {
       console.log(e)
     } finally {
       actions.setSubmitting(false)
     }
   }
+  const FormItemList = [
+    {
+      label: 'Base URL',
+      name: 'BaseURL',
+      required: true,
+      placeholder: process.env.BASE_URL,
+      prefix: <GlobalOutlined />,
+    },
+    {
+      label: 'Username',
+      name: 'Username',
+      required: true,
+      placeholder: 'Username',
+      prefix: <UserOutlined />,
+    },
+    {
+      label: 'Password',
+      name: 'Password',
+      required: true,
+      placeholder: 'Password',
+      prefix: <LockOutlined />,
+    },
+  ]
+  const FormItems = () => {
+    return FormItemList.map(
+      ({ label, required, name, placeholder, prefix }) => (
+        <FormItem label={label} name={name} required={required} key={name}>
+          <Input name={name} placeholder={placeholder} prefix={prefix} />
+        </FormItem>
+      )
+    )
+  }
 
   return (
     <div className="container">
       <Formik
-        initialValues={{ Username: "", Password: "" }}
+        initialValues={{
+          Username: '',
+          Password: '',
+          BaseURL: process.env.BASE_URL,
+        }}
         validationSchema={LoginSchema}
         onSubmit={onSubmit}
       >
         {() => (
           <Form layout="vertical">
-            <FormItem label="Username" name="Username" required={true}>
-              <Input name="Username" prefix={<UserOutlined />} />
-            </FormItem>
-
-            <FormItem label="Password" name="Password" required={true}>
-              <Input.Password name="Password" prefix={<LockOutlined />} />
-            </FormItem>
-
+            <FormItems />
             <div className="cta">
               <SubmitButton>Login</SubmitButton>
             </div>
